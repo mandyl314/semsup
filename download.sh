ebd87964823450739185d601968c2267110110da
Mandy Lin <mandyl@princeton.edu>
2022-11-02 10:23:43 -0400

    spanish val

--- download.sh
+++ download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-wget -O class_descrs.zip https://www.dropbox.com/s/1qrg5wteo4zp1ji/class_descrs.zip?dl=1
+# wget -O class_descrs.zip https://www.dropbox.com/s/1qrg5wteo4zp1ji/class_descrs.zip?dl=1
+wget -0 class_descrs.zip https://www.dropbox.com/s/eph4nr6qfvof413/class_descrs.zip?dl=1
 unzip class_descrs.zip
 rm -rf class_descrs.zip
