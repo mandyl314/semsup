6d4b228bda1a7c1588cdd478c3dc7879309a52ae
Mandy Lin <mandyl@princeton.edu>
2022-11-02 19:26:52 -0400

    new link

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # wget -O class_descrs.zip https://www.dropbox.com/s/1qrg5wteo4zp1ji/class_descrs.zip?dl=1
-wget -O class_descrs.zip https://www.dropbox.com/s/d6nqapowdbyymcx/class_descrs.zip?dl=1
+wget -O class_descrs.zip https://www.dropbox.com/s/kvxmwjznf1t1hf7/class_descrs.zip?dl=1
 unzip class_descrs.zip
 rm -rf class_descrs.zip
