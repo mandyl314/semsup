dba2376e1aa5cb0cffb4555b7f92aa48741db189
Mandy Lin <mandyl@princeton.edu>
2022-11-11 15:09:23 -0500

    both

--- translate_text.sh
+++ translate_text.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-data_dir=./class_descrs/awa
-#data_dir=./class_descrs_spanish/newsgroups
-output="./class_descrs/class_descrs_spanish/awa"
+# data_dir=./class_descrs/cifar/
+data_dir=./class_descrs/class_descrs_spanish/cifar
+# output="./class_descrs/class_descrs_spanish/awa"
 for entry in "$data_dir"/*
 do
-    FILENAME=$(echo "$entry" | sed "s/.*\///")
-    out_path="$output/$FILENAME"
-    python3 translate.py "$entry" > $out_path
-    # wc -l $entry
+    # FILENAME=$(echo "$entry" | sed "s/.*\///")
+    # out_path="$output/$FILENAME"
+    # python3 translate.py "$entry" > $out_path
+    wc -l $entry
 done
