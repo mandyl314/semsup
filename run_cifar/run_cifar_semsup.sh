646d4bb7d99a303a6345bed03053471b5c303b08
Mandy Lin <mandyl@princeton.edu>
2022-11-26 23:27:21 -0500

    overwrite cache

--- run_cifar/run_cifar_semsup.sh
+++ run_cifar/run_cifar_semsup.sh
@@ -5,8 +5,8 @@ RUN_SCRIPT="../run.py"
 CONFIG_FOLDER="./cifar_configs"
 
 commands=(
-    # "python $RUN_SCRIPT --config $CONFIG_FOLDER/cifar_scen1.yaml"
-    "python $RUN_SCRIPT --config $CONFIG_FOLDER/cifar_scen2.yaml"
+    "python $RUN_SCRIPT --config $CONFIG_FOLDER/cifar_scen1.yaml —data.args.overwrite_label_cache true"
+    # "python $RUN_SCRIPT --config $CONFIG_FOLDER/cifar_scen2.yaml"
     # "python $RUN_SCRIPT --config $CONFIG_FOLDER/cifar_scen3.yaml"
 )
 ARGS="--train --default_config $CONFIG_FOLDER/cifar_default.yaml --seed $SEED --name_suffix s$SEED"
