9bf2d266c71f5c3c7c4fe1070f65ad96312c5aab
Mandy Lin <mandyl@princeton.edu>
2022-12-24 21:22:00 -0500

    translate on colab

--- translate_text.sh
+++ translate_text.sh
@@ -4,7 +4,7 @@
 # data_dir="./class_descrs/class_descrs_spanish/cifar"
 data_dir="./class_descrs/cifar/google_cifar100_autoclean.labels"
 # output="./class_descrs/class_descrs_spanish/cifar"
-output="/content/"
+output="/content/semsup"
 for entry in "$data_dir"/*
 do
     FILENAME=$(echo "$entry" | sed "s/.*\///")
