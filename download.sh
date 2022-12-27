057556b0df18a856bc3041b83c7284c00443eacf
Mandy Lin <mandyl@princeton.edu>
2022-12-26 19:30:01 -0500

    rus sc2

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # wget -O class_descrs.zip https://www.dropbox.com/s/1qrg5wteo4zp1ji/class_descrs.zip?dl=1
-wget -O class_descrs.zip https://www.dropbox.com/s/v7005sv8wc1ou5l/class_descrs.zip?dl=1
+wget -O class_descrs.zip https://www.dropbox.com/s/kwsn9d6d0jegkwy/class_descrs.zip?dl=1
 unzip class_descrs.zip
 rm -rf class_descrs.zip
--- translate_text.sh
+++ translate_text.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-data_dir="./class_descrs/newsgroups"
-# data_dir="./class_descrs/class_descrs_russian/newsgroups"
-output="./class_descrs/class_descrs_russian/newsgroups"
+data_dir="./class_descrs/cifar"
+# data_dir="./class_descrs/class_descrs_russian/cifar"
+output="./class_descrs/class_descrs_russian/cifar"
 for entry in "$data_dir"/*
 do
     FILENAME=$(echo "$entry" | sed "s/.*\///")
     out_path="$output/$FILENAME"
-    python3 translate.py "$entry" > $out_path
+    # python3 translate.py "$entry" > $out_path
 
     wc -l $entry
+    wc -l $out_path
 done
