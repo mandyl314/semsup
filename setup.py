9a98d93857f2e3cec8283162fb0354a1ffa44514
Mandy Lin <mandyl@princeton.edu>
2023-01-28 18:14:22 -0500

    python

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
         'scikit-learn==1.0',
         'gensim==4.1.2',
         'jsonargparse==4.1.0',
-        'wandb==0.12.9'
+        'wandb==0.12.9',
+        'python==3.9.7'
     ],
 )
