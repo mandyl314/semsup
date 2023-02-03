afd9312631863f1be39d36b742105ac74bcb68c0
Mandy Lin <mandyl@princeton.edu>
2023-02-03 15:21:28 -0500

    data

--- download.sh
+++ download.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # wget -O class_descrs.zip https://www.dropbox.com/s/1qrg5wteo4zp1ji/class_descrs.zip?dl=1
-wget -O class_descrs.zip https://www.dropbox.com/s/yc9a6jyclrmm3ua/class_descrs.zip?dl=1
-unzip class_descrs.zip
-rm -rf class_descrs.zip
+# wget -O class_descrs.zip https://www.dropbox.com/s/yc9a6jyclrmm3ua/class_descrs.zip?dl=1
+wget -O class_descrs_small.zip https://www.dropbox.com/s/yqjtjo0vhd8pqdn/class_descrs_small.zip?dl=1
+# unzip class_descrs.zip
+# rm -rf class_descrs.zip
+unzip class_descrs_small.zip
+rm -rf class_descr_small.zip
