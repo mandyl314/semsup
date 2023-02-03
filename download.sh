0f08641da662c6e8be3c56a628988a427fb8ec48
Mandy Lin <mandyl@princeton.edu>
2023-02-03 14:16:53 -0500

    revert saved

--- download.sh
+++ download.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 # wget -O class_descrs.zip https://www.dropbox.com/s/1qrg5wteo4zp1ji/class_descrs.zip?dl=1
-<<<<<<< HEAD
 wget -O class_descrs.zip https://www.dropbox.com/s/yc9a6jyclrmm3ua/class_descrs.zip?dl=1
-=======
-wget -O class_descrs.zip https://www.dropbox.com/s/wo4trtpxt95997o/class_descrs.zip?dl=1
->>>>>>> parent of 8c0a9bf (arabic)
 unzip class_descrs.zip
 rm -rf class_descrs.zip
